ea8ea321b6171fa987a1dcfbcdae3dc2f28bc813
zjj <454550789@qq.com>
2018-03-09 18:21:58 +0800

    login

--- app.js
+++ app.js
@@ -4,13 +4,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+
+
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var playDetail =require('./routes/playDetail');
 
 var app = express();
-
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -26,6 +37,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/playDetail',playDetail)
+
+app.use(session({
+  secret :  'secret', // 对session id 相关的cookie 进行签名
+  resave : true,
+  saveUninitialized: false, // 是否保存未初始化的会话
+  cookie : {
+      maxAge : 1000 * 60 * 3, // 设置 session 的有效时间，单位毫秒
+  },
+}));
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,5 +66,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
+router.post('/login', function(req, res){
+  if(req.body.username == 'admin' && req.body.password == '123456'){
+      req.session.userName = req.body.username; // 登录成功，设置 session
+      res.json({ret_code : 0, ret_msg : '登录成功'});
+  }
+  else{
+      res.json({ret_code : 1, ret_msg : '账号或密码错误'});// 若登录失败，重定向到登录页面
+  }
+});
+router.post('/checklogin',function(req,res){
+	console.log(req.session);
+	res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+	res.header('Access-Control-Allow-Credentials', 'true')
+	if(req.session.userName){  //判断session 状态，如果有效，则返回主页，否则转到登录页面
+		res.json({ret_code : 1, ret_msg : '未登录'});
+	}else{
+		res.json({ret_code : 0, ret_msg : '登录成功'});
+	}
+})
 module.exports = router;
